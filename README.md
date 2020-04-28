befb0346597db172d434c06672f89fbaf2f1bb80
MY\yousei takei <yousei_san@icloud.com>
2020-04-28 19:02:57 +0900

    init commit

--- README.md
+++ README.md
@@ -1,68 +1,43 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# React Markdown with Material Design
+![img]
+Built with [React Markdown](https://github.com/rexxars/react-markdown)
+and [Material Design Bootstrap](https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design)
+![img]
 
-## Available Scripts
+# Table of Contents
+[Demo](#Demo)
+[Version](#Version)
+[Quick start](#Quick start)
+[Available commands](#Available commands)
+[How to install React Markdown and MDBReact via npm](#How to install React Markdown and MDBReact via npm)
+[Supported Browsers](#Supported Browsers)
+[Documentation](#Documentation)
+[Highlights](#Highlights)
+[Useful Links](#Useful Links)
+[Social Media](#Social Media)
 
-In the project directory, you can run:
+# Demo
+[TODO]()
 
-### `npm start`
+# Version
+- React 16.12.0
+- MDBReact 4.26.0
+- ReactMarkdown
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+# Quick start
+- `git clone https://github.com/tseijp/mdmd-react .`
+- `npm i`
+- `npm start`
+- open browser and visit [http://localhost:3000]
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+# How to install React Markdown and MDBReact via npm
+- create your project `create-react-app myapp`
+- `cd myapp`
+- `npm install --save mdbreact react-markdown`
+- Import style files into the src/index.js before the App.js file:
+```
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+```
+- Learn more : [](https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design#how-to-install-mdb-via-npm)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Mdmd           from './containers/Mdmd'          ;
+import Root           from './components/Root'          ;//root - Root container element that contains the rendered markdown
+import Text           from './components/Text'          ;//text - Text rendered inside of other elements, such as paragraphs
+import Break          from './components/Break'         ;//break - Hard-break (<br>)
+import Paragraph      from './components/Paragraph'     ;//paragraph - Paragraph (<p>)
+import Emphasis       from './components/Emphasis'      ;//emphasis - Emphasis (<em>)
+import Strong         from './components/Strong'        ;//strong - Strong/bold (<strong>)
+import ThematicBreak  from './components/ThematicBreak' ;//thematicBreak - Horizontal rule / thematic break (<hr>)
+import Blockquote     from './components/Blockquote'    ;//blockquote - Block quote (<blockquote>)
+import Delete         from './components/Delete'        ;//delete - Deleted/strike-through (<del>)
+import Link           from './components/Link'          ;//link - Link (<a>)
+import Image          from './components/Image'         ;//image - Image (<img>)
+import LinkReference  from './components/LinkReference' ;//linkReference - Link (through a reference) (<a>)
+import ImageReference from './components/ImageReference';//imageReference - Image (through a reference) (<img>)
+import Table          from './components/Table'         ;//table - Table (<table>)
+import TableHead      from './components/TableHead'     ;//tableHead - Table head (<thead>)
+import TableBody      from './components/TableBody'     ;//tableBody - Table body (<tbody>)
+import TableRow       from './components/TableRow'      ;//tableRow - Table row (<tr>)
+import TableCell      from './components/TableCell'     ;//tableCell - Table cell (<td>/<th>)
+import List           from './components/List'          ;//list - List (<ul>/<ol>)
+import ListItem       from './components/ListItem'      ;//listItem - List item (<li>)
+import Definition     from './components/Definition'    ;//definition - Definition (not rendered by default)
+import Heading        from './components/Heading'       ;//heading - Heading (<h1>-<h6>)
+import InlineCode     from './components/InlineCode'    ;//inlineCode - Inline code (<code>)
+import Code           from './components/Code'          ;//code - Block of code (<pre><code>)
+import Html           from './components/Html'          ;//html - HTML node (Best-effort rendering)
+import VirtualHtml    from './components/VirtualHtml'   ;//virtualHtml - When not using the HTML parser plugin, a cheap and dirty approach to supporting simple HTML elements without a complete parser.
+import ParsedHtml     from './components/ParsedHtml'    ;//parsedHtml - When using the HTML parser plugin, HTML parsed to a React element.
+export default Mdmd;
+export {
+    Root           as MdmdRoot,
+    Text           as MdmdText,
+    Break          as MdmdBreak,
+    Paragraph      as MdmdParagraph,
+    Emphasis       as MdmdEmphasis,
+    Strong         as MdmdStrong,
+    ThematicBreak  as MdmdThematicBreak,
+    Blockquote     as MdmdBlockquote,
+    Delete         as MdmdDelete,
+    Link           as MdmdLink,
+    Image          as MdmdImage,
+    LinkReference  as MdmdLinkReference,
+    ImageReference as MdmdImageReference,
+    Table          as MdmdTable,
+    TableHead      as MdmdTableHead,
+    TableBody      as MdmdTableBody,
+    TableRow       as MdmdTableRow,
+    TableCell      as MdmdTableCell,
+    List           as MdmdList,
+    ListItem       as MdmdListItem,
+    Definition     as MdmdDefinition,
+    Heading        as MdmdHeading,
+    InlineCode     as MdmdInlineCode,
+    Code           as MdmdCode,
+    Html           as MdmdHtml,
+    VirtualHtml    as MdmdVirtualHtml,
+    ParsedHtml     as MdmdParsedHtml,
+};
